35e1f5d0fc3ef1f3c3cdbb440a78890bf757c19e
Olena Lialiuk <81256578+olenalialiuk@users.noreply.github.com>
2022-05-11 12:22:50 +0000

    Week 4. Homework

--- week 4.py
+++ week 4.py
@@ -9,20 +9,40 @@ food=input("What is your favourite food? ")
 if food == "mushrooms":
     print("Wow, that’s my favorite too!")
 else:
-    print("n/")
+    print("\n")
 liked_food_in_common=[]
 liked_food_in_common.append(food)
 
 
 temperature=int(input("What is temperature now? "))
-if temperature < 72:
-    print("jacket should be worn.")
 forecast=input("What is forecast for today? ")
-if forecast == "showers" or "rain" or "snow":
-    print("umbrella should be brought.")
-elif temperature < 72 and forecast == "showers" or "rain" or "snow":
+if temperature < 72 and forecast != "showers" and forecast != "rain" and forecast != "snow":
+    print("Jacket should be worn.")
+elif temperature >= 72 and forecast == "showers" and forecast == "rain" and forecast == "snow":
+    print("Umbrella should be brought.")
+elif temperature < 72 and forecast == "showers" or forecast == "rain" or forecast == "snow":
     print("Based on the weather forecast, you should bring a jacket and an umbrella")
-elif temperature >= 72 and forecast == "showers" or "rain" or "snow":
+elif temperature >= 72 and forecast != "showers" or forecast != "rain" or forecast != "snow":
     print("Based on the weather forecast, you should bring an umbrella")
-elif temperature < 72:
-  print("Based on the weather forecast, you should bring a jacket. The weather is great! Have a nice day!")
+else:
+  print("The weather is great! Have a nice day!")
+print("\n")
+
+string_1 = len(input("Write the first string? "))
+string_2 = len(input("Write the second string? "))
+if string_1 == string_2:
+    print ("both strings have the same lenth")
+else:
+    print("The string 1 and string 2 are different.")
+
+fun_list=["Half-Life 2", "Grand Theft Auto V", "The Orange Box"]
+challeng_list=["Half-Life", "BioShock", "Grand Theft Auto V"]
+game=input("What is your favourite game? ")
+if game == fun_list and game == challeng_list:
+    print ("I think that one is fun and challenging!")
+elif game == fun_list and game != challeng_list:
+    print ("I think that one is fun!")
+elif game != fun_list and game == challeng_list:
+    print ("I think that one is challenging!")
+else:
+    print ("I don’t think that one is fun or challenging.")
